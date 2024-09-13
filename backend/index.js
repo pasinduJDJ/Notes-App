edd749774d3b5de8c8393c56e248e6ab01e320b0
pasinduJDJ <pasindudulanjanarj@gmail.com>
2024-09-13 21:17:10 +0530

    MongoDb Connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.connectionString);
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
+const jwt = require ("jsonwebtoken");
+const {authenticateToken } = require("./utilities");
 
 app.use(express.json());
 
@@ -13,6 +23,7 @@ app.use(
 app.get("/",(req,res)=>{
     res.json({ data:"hello"});
 });
+
 
 app.listen(8000);
 
